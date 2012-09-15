76321235d423c13ce6f7255438daa2f7e0af4978
zombah <zombah@gmail.com>
2012-09-16 00:06:20 +0400

    Add NEED_WORKAROUND_CORTEX_A9_745320
    As seen in betelgeuse setup
    
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,3 +10,5 @@ TARGET_KERNEL_CONFIG 			:= paz00_android_defconfig
 
 BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
 
+# Avoid generating of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 	:= true
