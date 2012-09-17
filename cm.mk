9e6881496905b6400b081e84f22d7ae416e459ca
zombah <zombah@gmail.com>
2012-09-17 13:45:12 +0400

    Bootanimation vars changed to cm10 style
    Signed-off-by: zombah <zombah@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,21 +1,20 @@
-# Release name                                                                                                          
-PRODUCT_RELEASE_NAME := ac100
-
+# Inherit my config
 $(call inherit-product, device/toshiba/ac100/full_ac100.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=paz00 BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.4/IMM76/292727:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.4 IMM76 292727 release-keys"
 
-PRODUCT_NAME := cm_ac100
-PRODUCT_RELEASE_NAME := ac100
-PRODUCT_DEVICE := ac100
-PRODUCT_BRAND := toshiba
-PRODUCT_MODEL := ac100
-PRODUCT_MANUFACTURER := toshiba
+PRODUCT_NAME 				:= cm_ac100
+PRODUCT_RELEASE_NAME 			:= ac100
+PRODUCT_DEVICE 				:= ac100
+PRODUCT_BRAND 				:= toshiba
+PRODUCT_MODEL 				:= ac100
+PRODUCT_MANUFACTURER 			:= toshiba
 
-TARGET_BOOTANIMATION_NAME := horizontal-1024x600
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD 		:= true
+TARGET_SCREEN_HEIGHT 			:= 1024
+TARGET_SCREEN_WIDTH 			:= 600
