b89e8575a7da5301f2417df77fb24c394eb561aa
zombah <zombah@gmail.com>
2012-11-05 21:39:18 +0400

    Remove duplicate kernel vars
    Signed-off-by: zombah <zombah@gmail.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,8 +2,5 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-TARGET_KERNEL_SOURCE := kernel/toshiba/paz00
-TARGET_KERNEL_CONFIG := paz00_android_defconfig
-
 # include the non-open-source counterpart to this file
 -include vendor/toshiba/paz00/AndroidBoardVendor.mk
