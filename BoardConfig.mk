a30ea4727f8ab65125426973a08d4562bac2332b
zombah <zombah@gmail.com>
2012-12-02 14:56:06 +0400

    Some kernel and cmdline fixes
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,13 +5,15 @@ TARGET_BOOTLOADER_BOARD_NAME 		:= paz00
 TARGET_OTA_ASSERT_DEVICE                := paz00,ac100,GT-P7510
 
 # Kernel
-TARGET_KERNEL_SOURCE 			:= kernel/toshiba/paz00
-TARGET_KERNEL_CONFIG 			:= cyanogenmod_paz00_defconfig
+#TARGET_KERNEL_SOURCE 			:= kernel/toshiba/paz00.test
+TARGET_KERNEL_SOURCE                   := kernel/toshiba/paz00
+#TARGET_KERNEL_CONFIG 			:= cyanogenmod_paz00_defconfig
+TARGET_KERNEL_CONFIG                   	:= paz00_android_defconfig
 
-#BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
+BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
 
 # cmdline with extended debug info
-BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
+#BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2 tegra_wdt.heartbeat=30
 
 # Jb new additions
 #
@@ -23,4 +25,11 @@ NEED_WORKAROUND_CORTEX_A9_745320	:= true
 
 #Use old kernel toolchain.  GCC 4.6 may be the root cause of our modem
 #interface dying all the time.
-TARGET_KERNEL_CUSTOM_TOOLCHAIN 		:= arm-eabi-4.4.3
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN 		:= arm-eabi-4.4.3
+
+# Use legacy camery istead Gallery2
+BOARD_USES_LEGACY_CAMERA 		:= true
+
+# Custom name for recovery
+RECOVERY_NAME 				:= Toshiba AC100 CWM-based Recovery
+
