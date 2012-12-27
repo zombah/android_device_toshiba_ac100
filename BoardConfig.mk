6760a21df2eb362cb7cf96215e41e4216be3b33f
zombah <zombah@gmail.com>
2012-12-27 14:32:21 +0400

    Add new mr1 vars
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,15 +10,13 @@ TARGET_KERNEL_SOURCE                   := kernel/toshiba/paz00
 #TARGET_KERNEL_CONFIG 			:= cyanogenmod_paz00_defconfig
 TARGET_KERNEL_CONFIG                   	:= paz00_android_defconfig
 
-BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
+#BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
 
 # cmdline with extended debug info
-#BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2 tegra_wdt.heartbeat=30
+BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2 tegra_wdt.heartbeat=30
 
-# Jb new additions
-#
 # Avoid generating of ldrcc instructions
-NEED_WORKAROUND_CORTEX_A9_745320	:= true
+#NEED_WORKAROUND_CORTEX_A9_745320	:= true
 
 # Use legacy ICS audio blobs
 #USES_AUDIO_LEGACY			:= true
@@ -28,11 +26,18 @@ NEED_WORKAROUND_CORTEX_A9_745320	:= true
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN 		:= arm-eabi-4.4.3
 
 # Use legacy camery istead Gallery2
-BOARD_USES_LEGACY_CAMERA 		:= true
+#BOARD_USES_LEGACY_CAMERA 		:= true
 
 # Custom name for recovery
 RECOVERY_NAME 				:= Toshiba AC100 CWM-based Recovery
 
 # Use tegra ICS video blobs
 BOARD_EGL_NEEDS_LEGACY_FB		:= true
+MAX_EGL_CACHE_KEY_SIZE 			:= 4096
+MAX_EGL_CACHE_SIZE 			:= 2146304
 
+BOARD_HAVE_MAGNETIC_SENSOR 		:= false
+BOARD_MOBILEDATA_INTERFACE_NAME 	:= "wwan0"
+
+# Use old audio hal
+COMMON_GLOBAL_CFLAGS			+= -DMR0_AUDIO_BLOB
