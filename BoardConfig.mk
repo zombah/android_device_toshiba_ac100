161bdeac9ca15b5d0ca623233c58fb0cc768dcad
zombah <zombah@gmail.com>
2013-05-10 04:02:16 +0400

    Add uboot custom image making script, some new vars for test and cleanup
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,15 +5,15 @@ TARGET_BOOTLOADER_BOARD_NAME 		:= paz00
 TARGET_OTA_ASSERT_DEVICE                := paz00,ac100,GT-P7510
 
 # Kernel
-#TARGET_KERNEL_SOURCE 			:= kernel/toshiba/paz00.test
-TARGET_KERNEL_SOURCE                   := kernel/toshiba/paz00
+TARGET_KERNEL_SOURCE                   := kernel/toshiba/ac100
 #TARGET_KERNEL_CONFIG 			:= cyanogenmod_paz00_defconfig
-TARGET_KERNEL_CONFIG                   	:= paz00_android_defconfig
+TARGET_KERNEL_CONFIG                   := paz00_android_debug_defconfig
+#TARGET_KERNEL_CONFIG                    := paz00_android_defconfig
 
-BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
+#BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00 tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2
 
 # cmdline with extended debug info
-#BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2 tegra_wdt.heartbeat=30
+BOARD_KERNEL_CMDLINE                    := mem=512M@0 console=ttyS0,115200n8 no_console_suspend=1 androidboot.hardware=paz00 ignore_loglevel earlyprintk initcall_debug tegrapart=recovery:300:a00:800,boot:d00:1000:800,mbr:1d00:200:800 nvtegra_hideparts=BCT,PT,EBT,MBR,EM1,EM2 tegra_wdt.heartbeat=30 cpufreq.debug=7
 
 # Jb new additions
 #
@@ -32,4 +32,13 @@ BOARD_USES_LEGACY_CAMERA 		:= true
 
 # Custom name for recovery
 RECOVERY_NAME 				:= Toshiba AC100 CWM-based Recovery
+
+# Uboot
+#BOARD_CUSTOM_BOOTIMG_MK 		:= device/toshiba/ac100/uboot-bootimg.mk
+
+# RIL
+BOARD_MOBILEDATA_INTERFACE_NAME 	:= "wwan0"
+
+# Test
+BOARD_FIRST_CAMERA_FRONT_FACING 	:= true
 
