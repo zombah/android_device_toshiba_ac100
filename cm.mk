0e4d5be6c86c44025e0477b7826d0de87ff77d5b
zombah <zombah@gmail.com>
2013-06-09 00:46:06 +0400

    Add 4.1 tervigon fingerprint
    Signed-off-by: zombah <zombah@gmail.com>

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, device/toshiba/ac100/full_ac100.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=paz00 BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.4/IMM76/292727:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.4 IMM76 292727 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=paz00 BUILD_FINGERPRINT="motorola/tervigon/wingray:4.1.1/JRO03H/405518:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.1.1 JRO03H 405518 release-keys"
 
 PRODUCT_NAME 				:= cm_ac100
 PRODUCT_RELEASE_NAME 			:= ac100
