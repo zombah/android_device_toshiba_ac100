b0c1ce6afedf8a75adf585aba710eb860af86958
zombah <zombah@gmail.com>
2013-10-22 16:26:34 +0400

    uboot: Add boot.scr to recovery zip
    Signed-off-by: zombah <zombah@gmail.com>

--- uboot-bootimg.mk
+++ uboot-bootimg.mk
@@ -53,19 +53,29 @@ endif #!TARGET_NO_KERNEL
 ifneq ($(strip $(TARGET_NO_RECOVERY)),true)
     INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
     recovery_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.img
+    recovery_bootscr := $(PRODUCT_OUT)/system/etc/boot.cmd
     
     RCV_INSTALLED_RAMDISK_TARGET := $(PRODUCT_OUT)/initrd-recovery.gz
 
-    RCV_INTERNAL_UMULTIIMAGE_ARGS := -A arm -O linux -T ramdisk -C none -a 0x40800000 -n "ramdisk"
+    RCV_INSTALLED_BOOTSCR_TARGET := $(PRODUCT_OUT)/boot.scr
+
+    RCV_INTERNAL_UMULTIIMAGE_ARGS := -A arm -O linux -T ramdisk -C none -a 0x40800000 -n "recovery-ramdisk"
 
     RCV_INTERNAL_UMULTIIMAGE_ARGS += -d $(PRODUCT_OUT)/ramdisk-recovery.img $(RCV_INSTALLED_RAMDISK_TARGET)
+
+    RCV_INTERNAL_BOOTSCR_ARGS := -A arm -O linux -T script -C none -a 0 -e 0 -n "recovery-script"
+
+    RCV_INTERNAL_BOOTSCR_ARGS += -d $(PRODUCT_OUT)/system/etc/boot.cmd $(RCV_INSTALLED_BOOTSCR_TARGET)
+
+$(RCV_INSTALLED_BOOTSCR_TARGET): $(MKIMAGE) $(recovery_bootscr)  
+			$(MKIMAGE) $(RCV_INTERNAL_BOOTSCR_ARGS)
 
 $(RCV_INSTALLED_RAMDISK_TARGET): $(MKBOOTIMG) $(INSTALLED_BOOTIMAGE_TARGET) $(MKIMAGE) $(recovery_ramdisk) $(INSTALLED_KERNEL_TARGET)
 			$(MKIMAGE) $(RCV_INTERNAL_UMULTIIMAGE_ARGS)
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(RCV_INSTALLED_RAMDISK_TARGET) $(INSTALLED_KERNEL_TARGET)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(RCV_INSTALLED_RAMDISK_TARGET) $(INSTALLED_KERNEL_TARGET) $(RCV_INSTALLED_BOOTSCR_TARGET)
 			$(hide) rm -f $@
-			zip -qDj $@ $(PRODUCT_OUT)/initrd-recovery.gz $(INSTALLED_KERNEL_TARGET)
+			zip -qDj $@ $(PRODUCT_OUT)/initrd-recovery.gz $(INSTALLED_KERNEL_TARGET) $(RCV_INSTALLED_BOOTSCR_TARGET)
 			@echo ----- Made recovery image \(zip\) -------- $@
 endif #!TARGET_NO_RECOVERY
 
