9f07457bcaa021b6229daf04acdd53e693d44931
zombah <zombah@gmail.com>
2013-11-03 18:43:23 +0400

    Makefile: Remove couple questionable permissions
    Signed-off-by: zombah <zombah@gmail.com>

--- ac100.mk
+++ ac100.mk
@@ -49,9 +49,7 @@ PRODUCT_COPY_FILES += device/toshiba/paz00-common/prebuild/boot.cmd:system/etc/b
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
