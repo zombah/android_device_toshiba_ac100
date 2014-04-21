e695111370d67fc577e40ecc4a60f4e35166dcc3
zombah <zombah@gmail.com>
2014-04-21 23:49:33 +0400

    Boardconfig: Switch to kernel config with nvec disabled
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,7 @@ TARGET_KERNEL_SOURCE                   := kernel/toshiba/ac100
 #TARGET_KERNEL_SOURCE                   := kernel/toshiba/marvin24s-kernel
 #TARGET_KERNEL_CONFIG 			:= cyanogenmod_paz00_defconfig
 #TARGET_KERNEL_CONFIG                   := paz00_android_debug_defconfig
-TARGET_KERNEL_CONFIG                    := paz00_android_defconfig
+TARGET_KERNEL_CONFIG                    := paz00_nonvec_defconfig
 #TARGET_KERNEL_CONFIG                   := paz00_android_debug_nonvec_defconfig
 
 BOARD_KERNEL_CMDLINE 			:= mem=512M@0 console=ttyS0,115200n8 androidboot.hardware=paz00
